b81e1715fd733d016101c2416c3cb2f67f2a9056
pesoto <psoto771@gmail.com>
2015-09-12 16:05:27 +0200

    multiclass example

---
+++ movie_review_demo.py
@@ -0,0 +1,52 @@
+import learner
+import machine_learning
+import pandas as pd
+import numpy as np
+
+### Import Data ###
+filename = 'review_data.csv'
+data = pd.read_csv(filename,encoding="utf-8")
+
+
+###################################################
+##Using Bag of Words (Word Vectors) as attributes##
+###################################################
+
+#Select 100 observations from the positive reviews
+#Select 100 observations from the negative reviews
+all_data = pd.concat([data.ix[0:100],data.ix[500:600]])
+#Initiate TextDoc object
+textData = machine_learning.TextDoc(all_data)
+#Generate Term Frequency-Inverse Document frequencies of Word Vectors (Note: this takes a bit of time)
+textData.tfidf()
+#Scale the features so each vector is of unit modulus
+textData.bagofwords = textData.tfidf_df.apply(lambda x: x/np.linalg.norm(x),1)
+#Include dummy variables for each class label in the dataframe
+#####1 - Positive#####
+#####0 - Negative#####
+textData.bagofwords["classLabel"] = pd.get_dummies(all_data['sentiment'])['pos']
+#Include the original text in the tfidf-dataframe
+textData.bagofwords["origText"] = all_data.text
+
+#Choose 1 from each class- positive & negative
+labeled_data = textData.bagofwords.loc[[0,500]]
+
+#Shuffle the remaining dataset
+shuffle = textData.bagofwords.loc[np.random.permutation(textData.bagofwords[~textData.bagofwords.index.isin([0,500])].index)]
+
+#Use 150 for the pool of unlabeled, and 50 for the test data
+unlabeled_data = shuffle[0:150]
+test_data = shuffle[150::]
+
+data1 = machine_learning.ActiveLearningDataset(labeled_data,classLabel="classLabel",origText="origText")
+data2 = machine_learning.ActiveLearningDataset(unlabeled_data,classLabel="classLabel",origText="origText")
+data3 = machine_learning.ActiveLearningDataset(test_data,classLabel="classLabel",origText="origText")
+
+active_learner = learner.learner(data1,test_datasets=data3,probability=0,NBC=True)
+length = len(data1.data)
+active_learner.pick_initial_training_set(length)
+active_learner.rebuild_models(undersample_first=True)
+
+active_learner.unlabeled_datasets.add_data(data2.data)
+
+active_learner.active_learn(10, num_to_label_at_each_iteration=2)
--- multiclass_learner_demo.py
+++ multiclass_learner_demo.py
@@ -49,75 +49,95 @@ classDummies = pd.get_dummies(temp_df.classLabel)
 #Create dictionary of binary active_learners (one for each class)
 classifiers = {}
 
-#Build one binary classifier for each column
-for col in classDummies.columns:
+#################################
+### CYCLE THROUGH ALL CLASSES ###
+#################################
+while True:
+	#Build one binary classifier for each column
+	for col in classDummies.columns:
 
-	#Make copies of the datasets
-	curr_labeledData = labeledData.copy()
-	curr_unlabeledData = unlabeledData.copy()
-	curr_testData = testData.copy()
+		#Make copies of the datasets
+		curr_labeledData = labeledData.copy()
+		curr_unlabeledData = unlabeledData.copy()
+		curr_testData = testData.copy()
 
-	#Overwrite the old classLabel with binary class labels
-	curr_labeledData['classLabel'] = classDummies[col].loc[curr_labeledData.index]
-	curr_unlabeledData['classLabel'] = classDummies[col].loc[curr_unlabeledData.index]
-	curr_testData['classLabel'] = classDummies[col].loc[curr_testData.index]
+		#Overwrite the old classLabel with binary class labels
+		curr_labeledData['classLabel'] = classDummies[col].loc[curr_labeledData.index]
+		curr_unlabeledData['classLabel'] = classDummies[col].loc[curr_unlabeledData.index]
+		curr_testData['classLabel'] = classDummies[col].loc[curr_testData.index]
 
-	data1 = machine_learning.ActiveLearningDataset(curr_labeledData,classLabel="classLabel",origText="origText")
-	data2 = machine_learning.ActiveLearningDataset(curr_unlabeledData,classLabel="classLabel",origText="origText")
-	data3 = machine_learning.ActiveLearningDataset(curr_testData,classLabel="classLabel",origText="origText")
+		data1 = machine_learning.ActiveLearningDataset(curr_labeledData,classLabel="classLabel",origText="origText")
+		data2 = machine_learning.ActiveLearningDataset(curr_unlabeledData,classLabel="classLabel",origText="origText")
+		data3 = machine_learning.ActiveLearningDataset(curr_testData,classLabel="classLabel",origText="origText")
 
-	#Create learner, with labeled dataset as initial training
-	active_learner = learner.learner(data1,test_datasets=data3,NBC=False,className = classDefinitions[col])
-	active_learner.load()
-	classifiers[col] = active_learner
-	#Confirm about to start new classifier:
-	while True:
-		var = raw_input('Would you like to continue building a classifier for class %s? \nY or N? \nAnswer:' % classDefinitions[col])
-		if var not in ('Y','N'):
-			print 'Choose either Y or N'
-			continue
-		else:
-			break
-	if var == 'N':
-		continue
-	length = len(data1.data)
-	active_learner.pick_initial_training_set(length)
-	active_learner.rebuild_models(undersample_first=True)
-
-	#Add the unlabeled data as dataset to query from
-	active_learner.unlabeled_datasets.add_data(data2.data)
-
-	#Remove any already labeled datapoints from choice set of unlabeled dataset 
-	to_remove = set(active_learner.unlabeled_datasets.data.index.values).intersection(active_learner.labeled_datasets.data.index.values)
-	active_learner.unlabeled_datasets.remove_instances(list(to_remove))
-
-	#Train 10 more observations, choosing among the most informative 
-	while True:
-		active_learner.active_learn(num_to_label, num_to_label_at_each_iteration=num_to_label)
+		#Create learner, with labeled dataset as initial training
+		active_learner = learner.learner(data1,test_datasets=data3,NBC=False,className = classDefinitions[col])
+		active_learner.load()
+		classifiers[col] = active_learner
+		#Confirm about to start new classifier:
 		while True:
-			var = raw_input('Continue training the %s classifier? \nY or N:' % classDefinitions[col])
+			var = raw_input('Would you like to continue building a classifier for class %s? \nY or N? \nAnswer:' % classDefinitions[col])
 			if var not in ('Y','N'):
 				print 'Choose either Y or N'
 				continue
 			else:
 				break
 		if var == 'N':
-			break
-		else:
 			continue
+		length = len(data1.data)
+		active_learner.pick_initial_training_set(length)
+		active_learner.rebuild_models(undersample_first=True)
 
-	#Add active learner to dictionary of binary active learners
-	classifiers[col] = active_learner
-	active_learner.save()
-	print "\n --------------------"
+		#Add the unlabeled data as dataset to query from
+		active_learner.unlabeled_datasets.add_data(data2.data)
 
-#Extract the decision value that the binary option is turned on (observation belongs to class k) 
-#		for each classifier
-final_preds = pd.DataFrame(columns=classDummies.columns,index=testData.index)
-for name,classifier in classifiers.items():
-	final_preds[name] = classifier.test_results['scores'][1]
+		#Remove any already labeled datapoints from choice set of unlabeled dataset 
+		to_remove = set(active_learner.unlabeled_datasets.data.index.values).intersection(active_learner.labeled_datasets.data.index.values)
+		active_learner.unlabeled_datasets.remove_instances(list(to_remove))
 
-#Take the maximum decision value and allocate the observation to the argument class k 
-#Print accuracy
-accuracy =  2*(final_preds.idxmax(1)-testData.classLabel).apply(lambda x: x==0).sum()
-print str(accuracy)+"% accuracy over all classes on test set"
+		#Train 10 more observations, choosing among the most informative 
+		while True:
+			active_learner.active_learn(num_to_label, num_to_label_at_each_iteration=num_to_label)
+			while True:
+				var = raw_input('Continue training the %s classifier? \nY or N:' % classDefinitions[col])
+				if var not in ('Y','N'):
+					print 'Choose either Y or N'
+					continue
+				else:
+					break
+			if var == 'N':
+				break
+			else:
+				continue
+
+		#Add active learner to dictionary of binary active learners
+		classifiers[col] = active_learner
+		active_learner.save()
+		print "***********************"
+		print "***********************"
+
+	#Extract the decision value that the binary option is turned on (observation belongs to class k) 
+	#		for each classifier
+	final_preds = pd.DataFrame(columns=classDummies.columns,index=testData.index)
+	for name,classifier in classifiers.items():
+		final_preds[name] = classifier.test_results['scores'][1]
+
+	#Take the maximum decision value and allocate the observation to the argument class k 
+	#Print accuracy
+	accuracy =  2*(final_preds.idxmax(1)-testData.classLabel).apply(lambda x: x==0).sum()
+	print str(accuracy)+"% accuracy over all classes on test set"
+	print "***********************"
+	print "***********************"
+	print "***********************"
+	print "***********************"
+	while True:
+		var = raw_input('You have cycled through all classes. Would you like to retrain the %s classifiers again? \nY or N:' % len(classDummies.columns))
+		if var not in ('Y','N'):
+			print 'Choose either Y or N'
+			continue
+		else:
+			break
+	if var == 'N':
+		break
+	else:
+		continue
